dde1224674c33c03bb398d1c098d53aa2d65b58c
Nobuyuki Sekimori <43375309+juno-rmks@users.noreply.github.com>
2026-02-14 08:41:16 +0900

    Refactor setup.py to use UPSTREAM path for marisa sources and include directories.

--- setup.py
+++ setup.py
@@ -10,24 +10,26 @@ if sys.platform == "win32":
 else:
     extra_compile_args += ["-std=c++14"]
 
+UPSTREAM = Path("third_party/marisa-trie/upstream")
+
 marisa_sources = [
-    *[str(f) for f in Path('marisa-trie/lib/marisa').rglob('*.cc')],
-    str(Path('marisa-trie/bindings/marisa-swig.i')),
-    str(Path('marisa-trie/bindings/marisa-swig.cxx')),
+    *[str(f) for f in (UPSTREAM / "lib/marisa").rglob("*.cc")],
+    str(UPSTREAM / "bindings/marisa-swig.i"),
+    str(UPSTREAM / "bindings/marisa-swig.cxx"),
 ]
 marisa_include_dirs = [
-    str(Path('marisa-trie/include')),
-    str(Path('marisa-trie/lib')),
-    str(Path('marisa-trie/bindings')),
+    str(UPSTREAM / "include"),
+    str(UPSTREAM / "lib"),
+    str(UPSTREAM / "bindings"),
 ]
 
 marisa = Extension(
-    name='marisa_bindings._marisa',
+    name="marisa_bindings._marisa",
     sources=marisa_sources,
     include_dirs=marisa_include_dirs,
-    language='c++',
+    language="c++",
     extra_compile_args=extra_compile_args,
-    swig_opts=['-c++'],
+    swig_opts=["-c++"],
 )
 
 setup(
